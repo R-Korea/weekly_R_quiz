28f3c14501e3e0a0d22136aaa7b9114e67ca7462
curycu <hancury@gmail.com>
2018-02-27 22:07:41 +0900

    add answer

---
+++ 201802/2.moving_average/moving_average_answer.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(scales)
+
+rm(list=ls())
+
+set.seed(20180225)
+
+n <- 36
+
+profit <- 
+  ts(
+    data=floor(100 * (sort(rnorm(n, 0, 1)) + rnorm(n, 0, .8))), 
+    start=c(2018, 1), 
+    frequency=12)
+
+ma <- function(x, window) filter(x=x, filter=rep(1/window, window))
+
+df <-
+  data.frame(
+    month = seq(as.Date(0, origin="2018-01-01"), length.out=n, by="1 month"), 
+    profit = as.numeric(profit),
+    profit.ma3 = ma(profit, 3)) 
+
+ggplot(data=df, aes(x=month, y=profit)) +
+  geom_line() +
+  geom_point() +
+  geom_line(aes(x=month, y=profit.ma3), colour='red', na.rm=TRUE) +
+  scale_x_date(date_labels='%Y-%m', date_breaks='3 month') +
+  theme_classic() +
+  labs(title='Monthly Profit: MA 3 month', x='')
+

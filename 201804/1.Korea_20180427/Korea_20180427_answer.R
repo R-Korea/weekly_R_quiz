067157429961ad29a426ae8034693051cb50b552
curycu <hancury@gmail.com>
2018-04-30 00:58:40 +0900

    add answer

---
+++ 201804/1.Korea_20180427/Korea_20180427_answer.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+
+korea <-
+  map_data('world') %>% 
+  filter(region %in% c('North Korea','South Korea'))
+
+united.korea <-
+  korea %>%
+  mutate(region = 'Korea')
+
+clean.theme <-
+  theme_bw() +
+  theme(
+    text = element_blank(),
+    line = element_blank(),
+    legend.position = 'none')
+
+p1 <-
+  ggplot(korea) +
+  geom_polygon(aes(x=long, y=lat, group=group, fill=region), colour=NA) +
+  scale_fill_manual(values=c('red','blue')) +
+  clean.theme
+
+p2 <-
+  ggplot(united.korea) +
+  geom_polygon(aes(x=long, y=lat, group=group, fill=region), colour=NA) +
+  scale_fill_manual(values=c('skyblue')) +
+  clean.theme
+
+grid.arrange(p1, p2, ncol=2)

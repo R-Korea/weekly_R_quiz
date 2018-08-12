e9be1770fda1ebd12103a4df9f6a4469468ae5f4
Ha, Hun Cheol <hancury@gmail.com>
2018-08-12 23:16:06 +0900

    Update purchase_chain_answer.R

--- 201802/1.purchase_chain/purchase_chain_answer.R
+++ 201802/1.purchase_chain/purchase_chain_answer.R
@@ -32,3 +32,6 @@ result # page column looks like <chr [4]> ...
 
 class(result) <- 'data.frame'
 result # page column show it's content well
+
+# facebook post
+# https://www.facebook.com/groups/KoreaRUsers/permalink/1223554364443930/
